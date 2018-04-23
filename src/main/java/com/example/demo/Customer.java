7463335bb598b2a35b0df6c2b4bc857ff0d239bf
Andreas Wälchli <andreas.waelchli@me.com>
2018-04-23 14:51:32 +0200

    pre-demo commit

---
+++ src/main/java/com/example/demo/Customer.java
@@ -0,0 +1,61 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Customer {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	private String firstName;
+	private String lastName;
+	private String town;
+
+	protected Customer() {
+		// JPA
+	};
+
+	public Customer(String firstName, String lastName, String town) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.town = town;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("Customer[id%d, firstName='%s', lastName='%s', town='%s']", id, firstName, lastName, town);
+	}
+
+	public Long getCustomerId() {
+		return id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getTown() {
+		return town;
+	}
+
+	public void setTown(String town) {
+		this.town = town;
+	}
+
+}
